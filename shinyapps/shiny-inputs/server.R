b4ed5eda91ab61e0b8f969d3eb97c73711d09670
Alejandro Lopez <jlopez@ingenieria-avanzada.com>
2021-10-25 20:49:41 -0600

    Agregados ejercicios de clase

---
+++ shinyapps/shiny-inputs/server.R
@@ -0,0 +1,44 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+  output$single_slider_out <- renderPrint({
+    input$slider_in
+  })
+  
+  output$multi_slider_out <- renderPrint({
+    input$slider_multi
+  })
+  
+  output$numeric_out <- renderPrint({
+    input$num_in
+  })
+  
+  output$single_date_out <- renderPrint({
+    input$single_date
+  })
+  
+  output$range_date_out <- renderPrint({
+    input$range_date
+  })
+  
+  output$chk_out <- renderPrint({
+    input$single_chk
+  })
+  
+  output$multi_chk_out <- renderPrint({
+    input$multi_chk
+  })
+  
+  output$radio_out <- renderPrint({
+    input$radio_in
+  })
+  
+  output$ab_out <- renderPrint({
+    input$action_btn
+  })
+  
+  output$al_out <- renderPrint({
+    input$action_link
+  })
+  
+})
